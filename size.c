08be0b4771f02b088488ae929502306817ebab4c
mathi1811 <68803069+mathi1811@users.noreply.github.com>
2020-12-05 11:17:29 +0530

    Create size.c
    finding the size of variables

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int intType;
+float floatType;
+char charType;
+double doubleType;
+printf("size of integer :%zu bytes\n",sizeof(intType));
+printf("size of floating point :%zu bytes\n",sizeof(floatType));
+printf("size of character :%zu bytes\n",sizeof(floatType));
+printf("size of double :%zu bytes\n",sizeof(floatType));
+return 0;
+}
