b5a0c68fa526715d49df51904ca436abd9f96da5
mathi1811 <68803069+mathi1811@users.noreply.github.com>
2020-12-05 11:20:36 +0530

    Create evenodd.c
    to check whether the given number is even or odd

---
+++ evenodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter a number:");
+scanf("%d",&num);
+if(num%2==0)
+  printf("%d is even",num);
+else
+  printf("%d is odd number",num);
+return 0;
+}
+  
+  
